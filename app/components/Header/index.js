f9eb23c7dee17ff1b46f822692dda1d6d36940af
GrayStrider <zhoga.ivan@gmail.com>
2019-04-20 21:55:25 +1100

    dropdown menu

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+import { Divider, Dropdown, Menu, Segment } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import NavBar from './NavBar';
 import HeaderLink from './HeaderLink';
 import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
+const MenuStyled = styled(Menu)`
+  margin-top: 0.6em !important;
+  & .menu {
+    margin-left: 0.6em !important;
+  }
+`
+
 class Header extends React.Component {
   render() {
     return (
-      <div>
-        <NavBar>
-          <HeaderLink to="/">
-            <FormattedMessage {...messages.home} />
-          </HeaderLink>
-          <HeaderLink to="/features">
-            <FormattedMessage {...messages.features} />
-          </HeaderLink>
-          <HeaderLink to="/playground">
-            <FormattedMessage {...messages.playground} />
-          </HeaderLink>
-        </NavBar>
-      </div>
-    );
+      <MenuStyled vertical>
+        <Dropdown pointing='left' item text='Pages'>
+          <Dropdown.Menu>
+            <Dropdown.Item link><a href='https:\\google.com' target='_blank'>Google</a></Dropdown.Item>
+            <Dropdown.Item link><Link to='/features'>Features</Link></Dropdown.Item>
+            <Dropdown.Item link><Link to='/playground'>Playground</Link></Dropdown.Item>
+            <Divider/>
+            <Dropdown.Item link><Link to='/'>Home</Link></Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Menu.Item link>
+          About
+        </Menu.Item>
+      </MenuStyled>
+    )
   }
 }
 
