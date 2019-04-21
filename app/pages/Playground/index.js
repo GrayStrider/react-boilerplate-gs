d44fb0c4c33b270abd17ee0191dae96681d2ec4e
GrayStrider <zhoga.ivan@gmail.com>
2019-04-21 17:05:33 +1100

    grid

--- app/pages/Playground/index.js
+++ app/pages/Playground/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Grid, Segment } from 'semantic-ui-react';
 
 const Wrapper = styled.div`
   background-color: cadetblue;
   height: 100%;
+  padding: 1em;
 `;
 
 function Playground(props) {
   return (
     <Wrapper>
-      Playground
-
+      <Grid columns={3} divided>
+        <Grid.Row stretched>
+          <Grid.Column>
+            <Segment>1</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>1</Segment>
+            <Segment>2</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>1</Segment>
+            <Segment>2</Segment>
+            <Segment>3</Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Wrapper>
   );
 }
