a58f48f81950a81f0cc75b4499bb96cf536b6a39
GrayStrider <zhoga.ivan@gmail.com>
2019-04-22 16:24:05 +1100

    created template

--- app/reducers.js
+++ app/reducers.js
@@ -9,12 +9,14 @@ import history from 'utils/history';
 import globalReducer from 'containers/App/reducer';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 import ticktickReducer from './pages/Ticktick/reducer';
+import defaultReducer from './__template/NewComponent/reducer';
 
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
 export default function createReducer(injectedReducers = {}) {
   const rootReducer = combineReducers({
+    default: defaultReducer,
     ticktick: ticktickReducer,
     global: globalReducer,
     language: languageProviderReducer,
