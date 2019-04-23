19dee8d582b8b38e8f79b9aba55b8f255e2f28c0
GrayStrider <zhoga.ivan@gmail.com>
2019-04-23 11:13:09 +1100

    spacing [FIX CHECKBOX MARK]

--- app/pages/Ticktick/components/TaskList/Task/styles.js
+++ app/pages/Ticktick/components/TaskList/Task/styles.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  .checkbox *:before {
+  margin: 1em;
+  
+  
+  .checkbox {
+    vertical-align: sub;
+    margin-right: 0.8em;
+  
+    & *:before {
     border: 1px solid ${props => ["gray", "lightblue", "yellow", "red"][props.priority]} !important;
     border-radius: 2px !important;
     background-color: black !important;
+    }
+  
+    & *:after {
+      color: white !important;
+    }
   }
 `;
 
