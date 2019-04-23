c787b62ca3fe837ede85ddc2d5dbdeddbe91ee9b
GrayStrider <zhoga.ivan@gmail.com>
2019-04-23 11:42:08 +1100

    mock store data

--- app/pages/Ticktick/mockDataReducer.js
+++ app/pages/Ticktick/mockDataReducer.js
@@ -3,41 +3,74 @@ import { DEFAULT_ACTION } from './actions';
 
 const tasks = [
   {
+    id: '0',
     content: 'Buy Milk',
     description: 'In the store',
     priority: 3,
-    completed: false
+    completed: false,
   },
   {
+    id: '1',
     content: 'Buy Milk',
     description: 'In the store',
     priority: 1,
-    completed: false
+    completed: false,
+    tags: ['1','2']
   },
   {
+    id: '2',
     content: 'Buy Milk',
     description: 'In the store',
     priority: 0,
-    completed: false
+    completed: false,
+    tags: ['1']
   },
   {
+    id: '3',
     content: 'Buy Milk',
     description: 'In the store',
     priority: 2,
-    completed: true
+    completed: true,
+    tags: []
   }
 
 ]
 
 const tags = [
   {
-    name: 'Home'
+    id: '1',
+    name: 'Home',
+    tasksWith: ['0', '2']
   },
   {
-    name: 'Work'
+    id: '2',
+    name: 'Work',
+    tasksWith: ['1']
   }
 ]
 
+
+const PredefinedGroups = [
+  {
+    id: 0,
+    name: 'Inbox',
+    tasksIn: ['3']
+  }
+]
+
+const Groups = [
+  {
+    id: 0,
+    name: 'Personal',
+    tasksIn: ['0', '2']
+  },
+  {
+    id: 1,
+    name: 'Vacation',
+    tasksIn: ['1']
+  }
+
+]
 export const initialState = {
   tasks,
   tags
